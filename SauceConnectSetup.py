aa46b40e5c2fff396e86569daa2f80e04a160352
Deniz Ozkaynak <dozkaynak@navinet.net>
2013-05-31 12:17:58 -0400

    Pushed SauceConnectSetup.py script, added README.txt file

---
+++ SauceConnectSetup.py
@@ -0,0 +1,61 @@
+#@author: Deniz Ozkaynak - Created: 05/30/2013 - Last Updated: 05/30/2013
+# Automatically installs the Sauce Connect Java utility to
+# create a secure tunnel to the Sauce Cloud.
+
+import subprocess, urllib, os, zipfile
+
+# Get a list of all open ports and make sure 80 & 443 are not proxied.
+def checkPorts():
+	print "Getting list of open ports..."
+	listOfPorts = subprocess.check_output('netstat -a | find "LISTENING"', shell=True)
+	if('80' in listOfPorts):
+		print "Port 80 open"
+	else:
+		print "Port 80 is NOT open"
+		
+	if('443' in listOfPorts):
+		print "Port 443 open"
+	else:
+		print "Port 443 is NOT open"
+
+# Download & unzip the Sauce Connect Java utility
+def downloadSauceConnect():
+	# The URL address of the Sauce Connect download
+	URL = 'http://saucelabs.com/downloads/Sauce-Connect-latest.zip'
+	# The directory we want to download the zip to (current working dir)
+	myDir = os.getcwd()
+	
+	name = os.path.join(myDir, 'temp.zip')
+	
+	# Try to retrieve the zip file
+	try:
+		name, hdrs = urllib.urlretrieve(URL, name)
+	except IOError, e:
+		print "Can't retrieve %r to %r: %s" % (URL, myDir, e)
+		return
+	
+	# Try to open the zip file
+	try:
+		myZip = zipfile.ZipFile(name)
+	except zipfile.error, e:
+		print "Bad zipfile (from %r): %s" % (URL, e)
+		return
+	
+	# Extract the contents of the zip file
+	myZip.extractall()
+	# Close/delete the zip file
+	myZip.close()
+	os.unlink(name)
+	
+# Start the Sauce Connect tunnel
+def startSauceConnect():
+	# Get the Sauce username from the user
+	SauceUsername = raw_input("Enter your Sauce Username: ")
+	# Get the Sauce API key from the user
+	SauceAPIKey = raw_input("Enter your Sauce API Key: ")
+	# Run the Java utility
+	subprocess.check_output('java -jar Sauce-Connect.jar ' + SauceUsername + " " + SauceAPIKey, shell=True)
+
+checkPorts()
+downloadSauceConnect()
+startSauceConnect()
